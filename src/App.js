0a822b76c6a2784e8a6fe3e8b703f9e8a857131d
Akhil <akhilgarg1610@gmail.com>
2022-09-16 00:09:12 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Check from './components/check'
+import Check2 from './components/Check2';
+function App() {
+  return (
+    <div className="App">
+      <Check2/>
+    </div>
+  );
+}
+
+export default App;
