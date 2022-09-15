9a6d21611d8f53e93b20c873c841678bafdf8fca
Akhil <akhilgarg1610@gmail.com>
2022-09-16 00:20:09 +0530

    r

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
+
 import React from 'react';
 import './App.css';
-import Check from './components/check'
+
 import Check2 from './components/Check2';
 function App() {
   return (
